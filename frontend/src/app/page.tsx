79d602e90ebd7ca60862eeaa6f6ed12fe688dbe6
clr <clrcera05@gmail.com>
2026-04-24 15:49:59 -0300

    navbar :D

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,18 +7,32 @@ export default function Home() {
   const [view, setView] = useState<"analyze" | "train">("analyze");
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-start py-16 px-16 bg-white dark:bg-black sm:items-start">
-        <select
-          className="border border-gray-300 rounded p-2 mb-4"
-          onChange={(e) => {
-            const value = e.target.value;
-            setView(value as "analyze" | "train");
+      <nav className="w-full flex items-center justify-between px-8 py-4 bg-white dark:bg-gray-900 shadow">
+        <div className="flex items-center gap-4">
+          <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+            RCPD Feature Viewer
+          </h1>
+        </div>
+        <div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          onClick={() => {
+            setView("analyze");
           }}
         >
-          <option value="analyze">Analyze</option>
-          <option value="train">Train</option>
-        </select>
-
+          Analyze Features
+        </button>
+        <button
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          onClick={() => {
+            setView("train");
+          }}
+        >
+          Train Model
+        </button>
+        </div>
+      </nav>
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-start py-16 px-16 bg-white dark:bg-black sm:items-start">
         {view === "analyze" && <AnalyzeFeatures />}
         {view === "train" && <Train />}
       </main>
